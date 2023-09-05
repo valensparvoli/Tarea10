569e79b198233439a21294193a0a26e797e167c4
Valensparvoli <valensparvoli@gmail.com>
2023-09-05 10:35:53 -0300

    Tarea 10

---
+++ script.js
@@ -0,0 +1,14 @@
+//Actividad 1
+
+var nombre= "Valentino";
+var apellido="Sparvoli";
+
+console.log(nombre + " " + apellido)
+
+//Actividad 2
+
+var num1=10;
+var num2=15;
+var suma= num1 + num2;
+var num3 = suma+ 10;
+console.log(num3)
